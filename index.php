15edeaf34ab3be0466f311e216fc9087bb7bd75e
Barasbi <betokov93@gmail.com>
2017-03-02 13:34:17 +0300

    add 1

--- index.php
+++ index.php
@@ -1,8 +1,16 @@
-﻿<?php require_once('includes/header.php'); ?>
-	
+<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_top.php'); ?>
+<?php require_once('includes/our.php'); ?>
+<?php require_once('includes/how.php'); ?>
+<?php require_once('includes/what.php'); ?>
+<?php require_once('includes/for_me.php'); ?>
+<?php require_once('includes/presonal.php'); ?>
+<?php require_once('includes/select.php'); ?>
+<?php require_once('includes/about.php'); ?>
+<?php require_once('includes/photo.php'); ?>
+<?php require_once('includes/find.php'); ?>
+<?php require_once('includes/footer.php'); ?>
 <?php require_once('includes/footer.php'); ?>
 
-	
 </body>
 </html>
-
