2feaec0cc8e995ef6a251fcc2582d523f90c27fc
ibragimtokmak <tokmakovibragim@gmail.com>
2017-03-02 20:03:23 +0300

    about

--- includes/about.php
+++ includes/about.php
@@ -1,7 +1,22 @@
-<section class="section section_padding">
+<section class="section section_padding about">
 <div class=" container">
 	<div class="row">
 		<div class="col-md-12">
+		    <div class="about-wrap">
+		        <p class="about-question">- отзывы у вас есть?</p>
+		        <p class="about-answer">- Письменных отзывов о нас много,их можно найти на различных источниках</p>
+		        <p class="about-note">"Но есть нюанс!":)</p>
+		        <p class="about-concurents">Беспринципные конкуренты регулярно пишут о нас всякую чушь на сайтах-отзовиках</p>
+		        <p class="about-saytruth">Сказать откровенно?</p>
+		        <p class="about-feedback"><span>90% всех</span> отзывов в интернете - <span>заказные</span>!</p>
+		        <p class="about-truth">Хотите узнать правду о садике - приезжайте в гости и лично поговорите с родителями.</p>
+		        <p class="about-watch">Или посмотрите видеоотзывы от реальных людей</p>
+		    </div>
+		    <div class="about-carousel" id="dgslider">
+		        <div class="about-item"><img src="img/video.png" alt=""></div>
+		        <div class="about-item"><img src="img/video.png" alt=""></div>
+		        <div class="about-item"><img src="img/video.png" alt=""></div>
+		    </div>
 		</div>
 	</div>
 </div>
