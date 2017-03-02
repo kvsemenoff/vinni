b20949f5cc7d5c9ce02fb04519f61288e09ddcfb
Berzeg07 <berezg86@gmail.com>
2017-03-02 14:22:01 +0300

    29.14

--- includes/how.php
+++ includes/how.php
@@ -1,7 +1,15 @@
 <section class="section section_how">
 	<div class="container">
 		<div class="dd-container">
-			<span class="dd-title">Как раскрыть</span>
+			<h2 class="dd-title">Как раскрыть</h2>
+			<div class="dd-slogan__h">
+				<span>Способности ребёнка?</span>
+			</div>
+			<div class="dd-slogan-2_h">
+				<span>Как понять, в каком деле <br>
+					он будет счастлив и успешен?
+				</span>
+			</div>
 		</div>
 	</div>
 </section>
