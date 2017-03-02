f76cf3b678014f480aa57102c558879290600244
Berzeg07 <berezg86@gmail.com>
2017-03-02 17:44:27 +0300

    29.14

--- includes/what.php
+++ includes/what.php
@@ -1,8 +1,48 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section dd-what-to-do_wrap">
+	<div class=" container">
+		<div class="dd-container">
+			<div class="dd-content_wt">
+				<span class="dd-wsp1_wt">Что делать, чтобы малыш не болел?</span>
+				<span class="dd-wsp2_wt">отвечаем: <span>проводим профилактику заболеваний</span></span><br>
+				<span class="dd-wsp3_wt">Благодаря чему ваши детки редко болеют:</span>
+				<ul>
+					<li>Допуск в детский сад только со справками от педиатра </li>
+					<li>Дорожки здоровья</li>
+					<li>Массаж "Волшебных точек" </li>
+					<li>Полоскание рта</li>
+					<li>Витаминные напитки</li>
+					<li>Гимнастика пробуждения</li>
+					<li>Кварцевание помещений</li>
+					<li>Осмотр медсестрой</li>
+					<li>Рекомендации родителям</li>
+				</ul>
+			</div>
 		</div>
 	</div>
-</div>
 </section>
+
+<section class="section dd-what-to-do_wrap_2">
+	<div class=" container">
+		<div class="dd-container dd-container__green">
+			<div class="dd-girl">
+				<img src="img/gf.png" alt="">
+			</div>
+			<div class="dd-content_wt dd-content_wt__green">
+				<span class="dd-wsp1_wt">Как будут кормить?</span>
+				<span class="dd-wsp2_wt">отвечаем: <span>кормим вкусно и полезно!</span></span><br>
+				<span class="dd-wsp3_wt">Все по Роспотребнадзору:</span>
+				<ul>
+					<li>Сбалансированное 5-разовое питание отвечает 
+						всем потребностям растущего организма, есть аллергостол 
+					</li>
+					<li>Качество продуктов и готовой пищи проверяется
+						директором и медицинской сестрой
+					</li>
+					<li>Повар 9 лет работает с детским питанием </li>
+				</ul>
+			</div>
+			<div class="clearfix"></div>
+		</div>
+	</div>
+</section>
+
