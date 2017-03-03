1339081b8ca8f09290f2c93c9e2cc27921a9b894
engine77 <zaur.1kager@gmail.com>
2017-03-03 14:27:45 +0300

    selectphp

--- includes/select.php
+++ includes/select.php
@@ -1,8 +1,160 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="section section_padding dfbgselect">
+	<div class=" container">
+		<div class="dfselect">
+			<h3 class="dfcapselect">Выберете подходящую вам <br><span>возрастную группу</span>
+			</h3>
+
+			<div class="dfkidblock">
+				<div class="dfkidleftbox">
+					<h4>ПчЁлки</h4>
+					<img src="img/kid1.png" alt="">
+				</div>
+				<div class="dfkidrightbox">
+					<div class="dftextall">
+						<div class="dfselecttext">
+							<h4>1 - 2,5 года</h4>	
+							<span>До 8 деток в группе</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>К 2,5 годам:</h4>	
+							<span>
+								Проявляют навыки опрятности <br>
+								Проговаривают слова и фразы <br>
+								Различают цвета <br>
+								Делятся увиденным <br>
+								Знакомы с ПДД
+							</span>	
+						</div>
+					</div>
+					<div class="dfselbutton">
+						<span class="dfmest">Осталось 1 место</span>
+						<a href="#" class="dfbron">забронировать</a>
+					</div>
+				</div>
+			</div>
+			<div class="dfkidblock">
+				<div class="dfkidleftbox">
+					<h4>НЕПОСЕДЫ</h4>
+					<img src="img/kid2.png" alt="">
+				</div>
+				<div class="dfkidrightbox">
+					<div class="dftextall">
+						<div class="dfselecttext">
+							<h4>2,5 - 4 года</h4>	
+							<span>До 12 деток в группе</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>К 4 годам:</h4>	
+							<span>
+								Могут играть небольшую роль <br>
+								Любят лепить и рисовать <br>
+								Умеют играть в мяч <br>
+								Считают до 10 								
+							</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>На английском языке:</h4>	
+							<span>								
+								Знают названия зверей, птиц,  <br>
+								фруктов и овощей. <br>
+								Уже в курсе, что «Ландан из <br>
+								зе кэпитал»								
+							</span>	
+						</div>
+					</div>
+					<div class="dfselbutton">
+						<span class="dfmest">Осталось 2 место</span>
+						<a href="#" class="dfbron">забронировать</a>
+					</div>
+				</div>
+			</div>
+			<div class="dfkidblock">
+				<div class="dfkidleftbox">
+					<h4>ПОЧЕМУЧКИ</h4>
+					<img src="img/kid3.png" alt="">
+				</div>
+				<div class="dfkidrightbox">
+					<div class="dftextall">
+						<div class="dfselecttext">
+							<h4>4 - 5 лет</h4>	
+							<span>До 12 деток в группе</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>К 5 годам:</h4>	
+							<span>
+								Соблюдают правила гигиены <br>
+								Складывают одежду <br>
+								Могут быть в роли  <br>
+								ведущего в играх <br>
+								Различают дорожные знаки  <br>
+								и сигналы светофора								
+							</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>На английском языке:</h4>	
+							<span>								
+								Знают названия частей лица, <br>
+								названия профессий, <br>
+								виды транспорта. <br>
+								Смотрят «Свинку Пеппу» <br>
+								в оригинале :)								
+							</span>	
+						</div>
+					</div>
+					<div class="dfselbutton">
+						<span class="dfmest">Осталось 2 место</span>
+						<a href="#" class="dfbron">забронировать</a>
+					</div>
+				</div>
+			</div>
+			<div class="dfkidblock">
+				<div class="dfkidleftbox">
+					<h4>звёздочки</h4>
+					<img src="img/kid4.png" alt="">
+				</div>
+				<div class="dfkidrightbox">
+					<div class="dftextall">
+						<div class="dfselecttext">
+							<h4>5 - 7 лет</h4>	
+							<span>До 12 деток в группе</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>К 7 годам:</h4>	
+							<span>
+								Могут  оценить игру актеров <br>
+								Подбирают синонимы  <br>
+								Знают алфавит, песни и стихи <br>
+								Прыгают через скакалку							
+							</span>	
+						</div>
+						<div class="dfselecttext dftxtbox2">
+							<h4>На английском языке:</h4>	
+							<span>								
+								Рассуждают о еде <br>
+								Говорят о погоде  <br>
+								Ориентируются в городе  <br>
+								Могут объяснить дорогу <br>
+								Знают, как будет по-английски  <br>
+								«Мама, я не хочу в школу!»							
+							</span>	
+						</div>
+					</div>
+					<div class="dfselbutton">
+						<span class="dfmest">Осталось 1 место</span>
+						<a href="#" class="dfbron">забронировать</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="dfselecttime">
+				<h3>И удобный вариант пребывания</h3>
+				<div class="dftime">8.00 - 13.00 <span>Адаптационный</span></div>
+				<div class="dftime">8.00 - 16.00 <span>Неполный</span></div>
+				<div class="dftime">8.00 - 18.00 <span>Полный </span></div>
+				<div class="dftime">8.00 - 20.00 <span>Продленный</span></div>
+				<h4>Запишитесь на <span>бесплатный</span> пробный день!</h4>
+				<a href="#" class="dfbron">записаться</a>
+			</div>
 		</div>
 	</div>
-</div>
 </section>
