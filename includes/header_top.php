10cb1322e85fe3984b02ed3609cf936cc0b408f9
Berzeg07 <berezg86@gmail.com>
2017-03-04 08:01:29 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -1,8 +1,36 @@
-<section class="section section_padding">
-<div class=" container">
-	<div class="row">
-		<div class="col-md-12">
+<section class="dd-header">
+	<div class=" container">
+		<div class="dd-header-container">
+			<div class="dd-top">
+				<div class="dd-top-box">
+					<img src="img/logo.png" alt="">
+				</div>
+				<div class="dd-top-box dd-top-box_1">
+					<span>Частный детский сад</span>
+					<span>с углубленным английским языком</span>
+				</div>
+				<div class="dd-top-box dd-top-box_2">
+					<span>+7 (812) 426-13-92</span>
+					<span>+7 (812) 988-42-74</span>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+			<div class="dd-top-main">
+				<span>Хотите, чтобы ваш малыш был счастлив, успешен<br> 
+					и свободно общался на английском?
+				</span>
+				<span>Мы создадим</span>
+				<span>индивидуальный 
+					путь к успеху
+				</span>
+				<span>вашего ребёнка!</span>
+			</div>
+			<div class="dd-top-bottom">
+				<span>Узнайте
+					всё о садике
+					за 2 минуты
+				</span>
+			</div>
 		</div>
 	</div>
-</div>
 </section>
---
+++ index2.php
@@ -0,0 +1,10 @@
+<?php require_once('includes/header.php'); ?>
+<?php require_once('includes/header_top.php'); ?>
+<?php require_once('includes/our.php'); ?>
+<?php require_once('includes/how.php'); ?>
+<?php require_once('includes/what.php'); ?>
+
+<?php require_once('includes/footer.php'); ?>
+
+</body>
+</html>
