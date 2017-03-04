a50456e1bea4cbc0d215d438964a67a663a99170
Berzeg07 <berezg86@gmail.com>
2017-03-04 09:33:30 +0300

    29.14

--- includes/header_top.php
+++ includes/header_top.php
@@ -26,10 +26,31 @@
 				<span>вашего ребёнка!</span>
 			</div>
 			<div class="dd-top-bottom">
-				<span>Узнайте
-					всё о садике
+				<span>Узнайте<br>
+					всё о садике<br>
 					за 2 минуты
 				</span>
+			</div>
+			<div class="dd-str">
+				<img src="img/str.png" alt="">
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="dd-video-header">
+	<div class="container">
+		<div class="dd-header-container">
+			
+			<div class="item-play">
+				<a href="#"><img src="img/play.png" alt=""></a>
+			</div>
+			<div class="dd-video-txt">
+				<span>Своему ребёнку родители хотят 
+					только самого лучшего
+				</span><br>
+				<span class="nsp1">А что будет лучшим для</span>
+				<span class="nsp2">его будущего?</span>
 			</div>
 		</div>
 	</div>
